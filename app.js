1fb06bc5781c97ae63bce583116fd08c5934b9f9
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-23 12:49:33 -0500

    completed commit

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Stephen";
+
+let lastName = "Cunningham";
+
+var age = 28;
+
+
 
 // Exercise 2
+let fullname = firstName + " " +lastName;
+
+console.log(fullname);
+
+console.log(`${firstName} ${lastName}`);
 
 // Exercise 3
+
+var city = "Birmingham";
+const pastTime = "Basketball";
+let myStory = `\Hi, I am ${fullname}. I live in ${city} and I enjoy playing ${pastTime} `;
+console.log(myStory);
